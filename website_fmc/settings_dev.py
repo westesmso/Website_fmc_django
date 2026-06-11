440409d116f16fa63f4b4f61c9b0227d960da273
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-06-11 11:57:50 -0300

    configurações de .env

---
+++ website_fmc/settings_dev.py
@@ -0,0 +1,7 @@
+"""
+Development settings.
+"""
+
+from .settings_base import *  # noqa: F401,F403
+
+DEBUG = True
---
+++ website_fmc/settings_prod.py
@@ -0,0 +1,14 @@
+"""
+Production settings.
+"""
+
+from .settings_base import *  # noqa: F401,F403
+
+DEBUG = False
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
